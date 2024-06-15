42ba02d5060e8578f1f7c3a5fa1be90f2ad50e02
Sharvil Nichat <sharvilnichat01@gmail.com>
2024-06-15 18:34:14 +0530

    Added changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,17 @@ import Carousel from './components/Carousel'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import {slides} from './data/carouselData.json'
+import {createBrowserRouter } from 'react-router-dom'
 
 function App() {
-
+ const router = createBrowserRouter([{
+  path:"/frontend/src/components/About.jsx",
+  element:<About/>
+ },{
+ }])
   return (
     <>
+    
       <Navbar />
       <Carousel data={slides}/>
       <About />
