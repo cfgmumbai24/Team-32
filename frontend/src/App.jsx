794d9de7d6ebcd3ce332009445584b5aa1f7825d
Sharvil Nichat <sharvilnichat01@gmail.com>
2024-06-15 19:25:06 +0530

    Added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,24 @@
 import './App.css'
-import About from './components/About'
-import Carousel from './components/Carousel'
+// import About from './components/About'
+// import Carousel from './components/Carousel'
 import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import {slides} from './data/carouselData.json'
-import { Routes,Route } from 'react-router-dom'
+// import Footer from './components/Footer'
+// import {slides} from './data/carouselData.json'
+import { Routes,Route, BrowserRouter } from 'react-router-dom'
+import Home from './components/pages/Home'
+import { Login } from './components/pages/Login'
 
 
 function App() {
 
   return (
-    <>
-    <Routes>
-      <Route path='./components/Login.jsx' element={<Login />} />
+    <BrowserRouter>
       <Navbar />
-      <Carousel data={slides}/>
-      <About />
-      <Footer/>
+    <Routes>
+      <Route exact path='/' element={<Home />}/>
+      <Route exact path='./Login' element={<Login />}/>
     </Routes>
-    </>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/components/pages/Login.jsx
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+import MailIcon from '@mui/icons-material/Mail';
+import LockIcon from '@mui/icons-material/Lock';
+import { Link, useNavigate } from 'react-router-dom';
+
+export const Login = () => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loginuser = async (e) => {
+    e.preventDefault();
+    const res = await fetch('/sigin?', {
+      method: 'POST',
+      headers: {
+        'content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    const data = await res.json();
+    if (data.status === 400 || !data) {
+      window.alert('Invalid credentials');
+    } else {
+      window.alert('Successful login');
+      navigate('/');
+    }
+  };
+
+  return (
+    <div>
+      <section className="h-screen flex justify-center bg-gray-100">
+        <div className="container max-w-md mx-auto p-4 pt-6 md:p-6 lg:p-12">
+          <div className="signin-image mb-4">
+            <figure>
+              <img src="/images/logo.svg" alt="registration pic" />
+            </figure>
+            <Link to="/Signup" className="text-blue-600 hover:text-blue-900">
+              Create new account
+            </Link>
+          </div>
+          <div className="signin-content">
+            <div className="signup-form">
+              <h2 className="form-title text-center">Sign in</h2>
+              <form className="register-form" id="register-form">
+                <div className="form-group mb-4">
+                  <label htmlFor="email">
+                    <MailIcon />
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autoComplete="off"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Your email address"
+                    className="w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </div>
+                <div className="form-group mb-4">
+                  <label htmlFor="password">
+                    <LockIcon />
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    autoComplete="off"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Your password"
+                    className="w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </div>
+                <div className="form-group form-button">
+                  <input
+                    type="submit"
+                    name="signup"
+                    id="signin"
+                    className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
+                    value="Login"
+                    onClick={loginuser}
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
